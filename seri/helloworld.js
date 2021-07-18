4f8c3005e69050ec549470d50444d20073da6617
colameo <serano@colameo.ch>
2021-07-18 20:09:06 +0000

    foo

--- seri/helloworld.js
+++ seri/helloworld.js
@@ -1,5 +1,9 @@
 var response = require("http/v4/response");
 
-response.println("Hello Serano");
+var name1 = "Serano2";
+var msg = `Hello ${name1}`;
+
+response.println(msg);
 response.flush();
 response.close();
+
